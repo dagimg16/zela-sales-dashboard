dbb9dd8cade94564549155d4f2db3b0df99aea49
Francisco Gonzalez <FRANCISCOG361@aztecamilling.com>
2025-04-09 16:02:14 -0500

    added the dropdown population functions

--- static/js/script.js
+++ static/js/script.js
@@ -13,26 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.getElementById("yearDropDown").addEventListener("change", (e) => {
         yearChanged(e.target.value);
-    }); 
+    });
 
     document.getElementById("monthDropDown").addEventListener("change", (e) => {
         monthChanged(e.target.value);
-    });     
+    });
 
     changeFiltersAndDashboard(selectedStore, selectedYear, selectedMonth);
 
 });
 
-function changeFiltersAndDashboard(store, year, month){
+function changeFiltersAndDashboard(store, year, month) {
     selectedStore = store;
     selectedYear = year;
     selectedMonth = month;
 
-    fetch('/api/stores').then(res => res.json()).then(stores =>{
+    fetch('/api/stores').then(res => res.json()).then(stores => {
         populateStoreDropdown(stores, store)
     });
 
-    fetch(`/api/years?store=${store}`).then(res => res.json()).then(years =>{
+    fetch(`/api/years?store=${store}`).then(res => res.json()).then(years => {
         const yearList = years.map(obj => obj.years);
         const yearToUse = yearList.includes(year) ? year : yearList[0]
         selectedYear = yearToUse
@@ -41,14 +41,41 @@ function changeFiltersAndDashboard(store, year, month){
 
         return fetch(`/api/months?store=${store}&year=${yearToUse}`);
     })
-    .then(res => res.json()).then(months =>{
-        const monthList = months.map(obj => obj.months);
-        const monthToUse = monthList.includes(month) ? month : monthList[0];
-        selectedMonth = monthToUse;
-        
-        populateMonthDropdown(monthList, monthToUse);
+        .then(res => res.json()).then(months => {
+            const monthList = months.map(obj => obj.months);
+            const monthToUse = monthList.includes(month) ? month : monthList[0];
+            selectedMonth = monthToUse;
 
-        updateDashboard(selectedStore, selectedYear, selectedMonth);
-    });
+            populateMonthDropdown(monthList, monthToUse);
 
+            updateDashboard(selectedStore, selectedYear, selectedMonth);
+        });
+
+}
+function populateStoreDropdown(data, defualtStore) {
+    const selector = d3.select("#storeDropDown")
+    selector.selectAll("option").remove();
+    data.forEach((options) => {
+        selector.append("option")
+            .text(options.store).attr("value", options.store)
+            .property("selected", options.store == defualtStore);
+    })
+}
+function populateYearDropdown(data, defualtYear) {
+    const selector = d3.select("#yearDropDown")
+    selector.selectAll("option").remove();
+    data.forEach((options) => {
+        selector.append("option")
+            .text(options).attr("value", options)
+            .property("selected", options == defualtYear)
+    })
+}
+function populateMonthDropdown(data, defualtMonth) {
+    const selector = d3.select("#monthDropDown")
+    selector.selectAll("option").remove();
+    data.forEach((options) => {
+        selector.append("option")
+            .text(options).attr("value", options)
+            .property("selected", options == defualtMonth)
+    })
 }
